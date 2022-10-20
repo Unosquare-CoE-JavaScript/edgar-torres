c0783d7e6d1759653d8db0b3bc6306db69cf3799
Edgar Torres <edgar.torres@COL-MC-220701A.local>
2022-10-19 22:24:59 -0500

    TDD and basic testing guidelines

--- Module1.6/testing101.js
+++ Module1.6/testing101.js
@@ -37,27 +37,93 @@ Functional testing:
 - More difficult to debug failing tests
 */
 
-// Unit testing example with Jest 
+// Unit testing example with Jest
 
 const mathOperations = {
-    sum: function(a,b) {
-        return a+b;
-    },
-    diff: function(a,b) {
-        return a-b;
-    },
-    product: function(a,b) {
-        return a*b;
-    }
-}
+  sum: function (a, b) {
+    return a + b;
+  },
+  diff: function (a, b) {
+    return a - b;
+  },
+  product: function (a, b) {
+    return a * b;
+  },
+};
 
-module.exports = mathOperations
+module.exports = mathOperations;
 
 // The test
 
 describe("Calulator tests", () => {
-    TextDecoderStream('adding 1+2 should return 3', () => {
-        expect(mathOperations.sum(1,2)).toBe(3);
-    });
-})
+  TextDecoderStream("adding 1+2 should return 3", () => {
+    expect(mathOperations.sum(1, 2)).toBe(3);
+  });
+});
 
+// A different example of a test in React
+
+import { render, screen } from "@testing-library/react";
+import App from './App';
+
+test('button has correct initial colour', () => {
+  render (<App />);
+
+  //find an element with a role of button and text of 'Change to blue'
+  const colourButton = screen.getByRole('button', {name: 'Change to blue'});
+  expect(colourButton).toHaveStyle({backgroundColor: 'red'})
+});
+
+test('button turns blue when clicked', () => {
+  render (<App />);
+  const colourButton = screen.getByRole('button', {name: 'Change to blue'}); 
+  fireEvent.click(colourButton); //Imported from testing library 
+  expect(colourButton).toHaveStyle({backgroundColor: 'blue'});
+  expect(colourButton).toHaveTextContent('Change to red'); 
+});
+
+// Prop tip: All expectations shouldn't be in the same test file
+
+// Unit Testing Functions
+/* 
+It's adviseable to unit test if:
+- Code is too complex or simply difficult to test via functional tests
+- Too many edge cases
+- Useful to determined what caused functional tests to fail
+*/
+
+// ESLint and Prettier
+
+/* 
+ESLint:
+- Linter: Analyzes static text and marks syntax that breaks rules
+- Static: Analyzes code as written, not what happens when code is run
+- Linting keeps code style consistent, especially for multi-eng projects
+- Catches errors in code
+- Testing library and jest-dom ESlint plugins enforce best practices
+
+Prettier:
+- Formatters automatically format code (indents, spacing), like adding missing space around curly braces.
+*/
+
+/*
+Useful Debugging tips:
+- screen.debut()
+- import logRoles from the testing library
+*/
+
+/*
+Useful testing tips:
+- Test small pieces of code in isolation
+- Follow Arrange, Act, Assert = Arrange thins for testing, then acting upon it and then assert if the hypothesis is correct
+- Keep them short
+- Make them simple
+- Test edge cases, as in test if the code throws exceptions when it should, for example.
+- Write tests before fixing bugs
+- Make them performant
+- Keep them stateless
+- Write deterministic tests
+- Use descriptive names
+- Test one requirement at a time
+- Run tests automatically
+*/
